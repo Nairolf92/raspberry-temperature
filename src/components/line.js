3aea40c20ef231947064a2b9743185efcf27d385
Florian Kelnerowski <f.kelnerowski@gmail.com>
2018-11-11 12:45:22 +0100

    first commit

---
+++ src/components/line.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import {Line} from 'react-chartjs-2';
+import { FaAngleLeft, FaAngleRight} from 'react-icons/fa'; 
+import dataTemp from '../data.json';
+
+export class LineCustom extends React.Component {
+  constructor(props) {
+    super(props);
+    // current date = new date toujours pour le calcul des jours
+    // display date = pour la recherche json et affichage
+    var currentDate = new Date();
+    var displayDate = currentDate.toLocaleDateString('fr-FR').slice(0,10);
+    
+    //console.log("currentdate"+currentDate)
+    //console.log("displaydate"+displayDate)
+    // This binding is necessary to make `this` work in the callback
+    this.goToPreviousDay = this.goToPreviousDay.bind(this, currentDate); 
+    this.goToNextDay = this.goToNextDay.bind(this, currentDate); 
+    this.state = {
+      arrayDate : this.getArrayOfDate(dataTemp, displayDate),
+      tempDate : this.getArrayOfTemp(dataTemp, displayDate),
+      currentDate : currentDate,
+      displayDate : displayDate
+    }
+  }
+
+  goToPreviousDay(currentDate){
+    //console.log("goToPreviousDay");
+    currentDate.setDate(currentDate.getDate()-1)
+    //console.log("currentDate : "+currentDate)
+    let displayDate = currentDate.toLocaleDateString('fr-FR')
+    //console.log("displayDate : "+displayDate)
+    this.setState(state => ({
+      currentDate : currentDate,
+      displayDate : displayDate,
+      arrayDate : this.getArrayOfDate(dataTemp, displayDate),
+      tempDate : this.getArrayOfTemp(dataTemp, displayDate),
+    }));
+    //console.log("this.state.currentDate : "+this.state.currentDate)
+    //console.log("this.state.displayDate : "+this.state.displayDate)
+    //console.log("END goToPreviousDay")
+  }
+
+  goToNextDay(currentDate){
+    currentDate.setDate(currentDate.getDate()+1)
+    let displayDate = currentDate.toLocaleDateString('fr-FR')
+    this.setState(state => ({
+      currentDate : currentDate,
+      displayDate : displayDate,
+      arrayDate : this.getArrayOfDate(dataTemp, displayDate),
+      tempDate : this.getArrayOfTemp(dataTemp, displayDate),
+    }));
+  }
+
+  getArrayOfDate(dataTemp, displayDate)
+  {
+    // Old way to process
+    let data = [];
+    for (var key in dataTemp) {
+      //console.log(key.substring(0,10))
+      if(key.substring(0,10) == displayDate) {
+        //console.log(key)
+        data.push(key)
+      }
+    }
+    return data
+  }
+
+  getArrayOfTemp(dataTemp, displayDate)
+  {
+    // ES6 format 
+    let data = [];
+    Object.entries(dataTemp).forEach(([key, value]) => {
+      if(key.substring(0,10) == displayDate)
+        data.push(value)
+    })
+    return data
+  }
+
+  getFormattedDate(d) {
+    let month = String(d.getMonth() + 1);
+    let day = String(d.getDate());
+    let year = String(d.getYear());
+    year = year -100;
+  
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+  
+    return `${day}-${month}-${year}`;
+  }
+
+  render() {
+    const data = {
+      labels : this.state.arrayDate,
+      datasets: [
+        {
+          label: 'Raspberry temperature (°C)',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          data: this.state.tempDate
+        }
+      ] 
+    };
+
+    return (
+      <div>
+        <span class="changeDate">
+          <FaAngleLeft size='3em' onClick={this.goToPreviousDay}/>
+          <h2>{this.state.displayDate}</h2>
+          <FaAngleRight size='3em' onClick={this.goToNextDay}/>
+        </span>
+
+        <Line data={data} />
+      </div>
+    );
+  }
+};
+
+export default LineCustom;
